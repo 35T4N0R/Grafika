d92b5847ecfc88513d2f541a0081de64373ac06d
Michał Zaorski <michal.zaorski98@gmail.com>
2020-10-31 12:25:04 +0100

    conversion is working i guess with sliders and text box, add some events to handle text box focus and set flags

--- Grafika/ThirdPage.xaml.cs
+++ Grafika/ThirdPage.xaml.cs
@@ -22,23 +22,26 @@ namespace Grafika
 
         bool rgbDragged = false;
         bool cmykDragged = false;
+        bool rgbTyping = false;
+        bool cmykTyping = false;
 
         public ThirdPage()
         {
             InitializeComponent();
         }
-//        Black = minimum(1-Red,1-Green,1-Blue)
-//Cyan = (1-Red-Black)/(1-Black)
-//Magenta = (1-Green-Black)/(1-Black)
-//Yellow = (1-Blue-Black)/(1-Black)
 
-//Red = 1-minimum(1, Cyan*(1-Black)+Black)
-//Green = 1-minimum(1, Magenta*(1-Black)+Black)
-//Blue = 1-minimum(1, Yellow*(1-Black)+Black)
+        //formulas
+        //Black = minimum(1-Red,1-Green,1-Blue)
+        //Cyan = (1-Red-Black)/(1-Black)
+        //Magenta = (1-Green-Black)/(1-Black)
+        //Yellow = (1-Blue-Black)/(1-Black)
+
+        //Red = 1-minimum(1, Cyan*(1-Black)+Black)
+        //Green = 1-minimum(1, Magenta*(1-Black)+Black)
+        //Blue = 1-minimum(1, Yellow*(1-Black)+Black)
 
 
-
-
+        #region helper functions
         private double CheckIfProperValue(double value)
         {
             if(value < 0 || double.IsNaN(value))
@@ -48,32 +51,67 @@ namespace Grafika
 
             return value;
         }
-
+        
         private void FillRectangles()
         {
+            if(RGBRec == null || CMYKRec == null)
+            {
+                return;
+            }
 
             RGBRec.Fill = new SolidColorBrush(Color.FromRgb((byte)(red * 255), (byte)(green * 255), (byte)(blue * 255)));
             CMYKRec.Fill = new SolidColorBrush(Color.FromRgb((byte)(red * 255), (byte)(green * 255), (byte)(blue * 255)));
         }
 
+        private void CalculateCMYK()
+        {
+            red = RSlider != null ? RSlider.Value / 255 : 0;
+            green = GSlider != null ? GSlider.Value / 255 : 0;
+            blue = BSlider != null ? BSlider.Value / 255 : 0;
 
+            blackKey = CheckIfProperValue(Math.Min(Math.Min(1 - red, 1 - green), 1 - blue));
+            cyan = CheckIfProperValue((1 - red - blackKey) / (1 - blackKey));
+            magenta = CheckIfProperValue((1 - green - blackKey) / (1 - blackKey));
+            yellow = CheckIfProperValue((1 - blue - blackKey) / (1 - blackKey));
+
+            KSlider.Value = Math.Round(blackKey * 100, MidpointRounding.AwayFromZero);
+            CSlider.Value = Math.Round(cyan * 100, MidpointRounding.AwayFromZero);
+            MSlider.Value = Math.Round(magenta * 100, MidpointRounding.AwayFromZero);
+            YSlider.Value = Math.Round(yellow * 100, MidpointRounding.AwayFromZero);
+        }
+
+        private void CalculateRGB()
+        {
+            cyan = CSlider != null ? CSlider.Value / 100 : 0;
+            magenta = MSlider != null ? MSlider.Value / 100 : 0;
+            yellow = YSlider != null ? YSlider.Value / 100 : 0;
+            blackKey = KSlider != null ? KSlider.Value / 100 : 0;
+
+            red = CheckIfProperValue((1 - cyan) * (1 - blackKey));
+            green = CheckIfProperValue((1 - magenta) * (1 - blackKey));
+            blue = CheckIfProperValue((1 - yellow) * (1 - blackKey));
+
+            RSlider.Value = Math.Round(red * 255, MidpointRounding.AwayFromZero);
+            GSlider.Value = Math.Round(green * 255, MidpointRounding.AwayFromZero);
+            BSlider.Value = Math.Round(blue * 255, MidpointRounding.AwayFromZero);
+        }
+        private bool CheckIfSlidersLoaded()
+        {
+
+            if (RSlider == null || GSlider == null || BSlider == null || CSlider == null || MSlider == null || YSlider == null || KSlider == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+        #region slider value changed
         private void RSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if (rgbDragged)
             {
-                red = RSlider.Value / 255;
-                green = GSlider.Value / 255;
-                blue = BSlider.Value / 255;
-
-                blackKey = CheckIfProperValue(1 - Math.Max(Math.Max(red, green), blue));
-                cyan = CheckIfProperValue((1 - red - blackKey) / (1 - blackKey));
-                magenta = CheckIfProperValue((1 - green - blackKey) / (1 - blackKey));
-                yellow = CheckIfProperValue((1 - blue - blackKey) / (1 - blackKey));
-
-                KSlider.Value = (int)(blackKey * 100);
-                CSlider.Value = (int)(cyan * 100);
-                MSlider.Value = (int)(magenta * 100);
-                YSlider.Value = (int)(yellow * 100);
+                CalculateCMYK();
 
                 FillRectangles();
             }
@@ -84,19 +122,7 @@ namespace Grafika
         {
             if (rgbDragged)
             {
-                red = RSlider.Value / 255;
-                green = GSlider.Value / 255;
-                blue = BSlider.Value / 255;
-
-                blackKey = CheckIfProperValue(1 - Math.Max(Math.Max(red, green), blue));
-                cyan = CheckIfProperValue((1 - red - blackKey) / (1 - blackKey));
-                magenta = CheckIfProperValue((1 - green - blackKey) / (1 - blackKey));
-                yellow = CheckIfProperValue((1 - blue - blackKey) / (1 - blackKey));
-
-                KSlider.Value = (int)(blackKey * 100);
-                CSlider.Value = (int)(cyan * 100);
-                MSlider.Value = (int)(magenta * 100);
-                YSlider.Value = (int)(yellow * 100);
+                CalculateCMYK();
 
                 FillRectangles();
             }
@@ -106,19 +132,7 @@ namespace Grafika
         {
             if (rgbDragged)
             {
-                red = RSlider.Value / 255;
-                green = GSlider.Value / 255;
-                blue = BSlider.Value / 255;
-
-                blackKey = CheckIfProperValue(1 - Math.Max(Math.Max(red, green), blue));
-                cyan = CheckIfProperValue((1 - red - blackKey) / (1 - blackKey));
-                magenta = CheckIfProperValue((1 - green - blackKey) / (1 - blackKey));
-                yellow = CheckIfProperValue((1 - blue - blackKey) / (1 - blackKey));
-
-                KSlider.Value = (int)(blackKey * 100);
-                CSlider.Value = (int)(cyan * 100);
-                MSlider.Value = (int)(magenta * 100);
-                YSlider.Value = (int)(yellow * 100);
+                CalculateCMYK();
 
                 FillRectangles();
             }
@@ -128,18 +142,7 @@ namespace Grafika
         {
             if (cmykDragged)
             {
-                cyan = CSlider.Value / 100;
-                magenta = MSlider.Value / 100;
-                yellow = YSlider.Value / 100;
-                blackKey = KSlider.Value / 100;
-
-                red = (1 - cyan) * (1 - blackKey);
-                green = (1 - magenta) * (1 - blackKey);
-                blue = (1 - yellow) * (1 - blackKey);
-
-                RSlider.Value = (int)(red * 255);
-                GSlider.Value = (int)(green * 255);
-                BSlider.Value = (int)(blue * 255);
+                CalculateRGB();
 
                 FillRectangles();
             }
@@ -149,18 +152,7 @@ namespace Grafika
         {
             if (cmykDragged)
             {
-                cyan = CSlider.Value / 100;
-                magenta = MSlider.Value / 100;
-                yellow = YSlider.Value / 100;
-                blackKey = KSlider.Value / 100;
-
-                red = (1 - cyan) * (1 - blackKey);
-                green = (1 - magenta) * (1 - blackKey);
-                blue = (1 - yellow) * (1 - blackKey);
-
-                RSlider.Value = (int)(red * 255);
-                GSlider.Value = (int)(green * 255);
-                BSlider.Value = (int)(blue * 255);
+                CalculateRGB();
 
                 FillRectangles();
             }
@@ -170,18 +162,7 @@ namespace Grafika
         {
             if (cmykDragged)
             {
-                cyan = CSlider.Value / 100;
-                magenta = MSlider.Value / 100;
-                yellow = YSlider.Value / 100;
-                blackKey = KSlider.Value / 100;
-
-                red = (1 - cyan) * (1 - blackKey);
-                green = (1 - magenta) * (1 - blackKey);
-                blue = (1 - yellow) * (1 - blackKey);
-
-                RSlider.Value = (int)(red * 255);
-                GSlider.Value = (int)(green * 255);
-                BSlider.Value = (int)(blue * 255);
+                CalculateRGB();
 
                 FillRectangles();
             }
@@ -191,23 +172,13 @@ namespace Grafika
         {
             if (cmykDragged)
             {
-                cyan = CSlider.Value / 100;
-                magenta = MSlider.Value / 100;
-                yellow = YSlider.Value / 100;
-                blackKey = KSlider.Value / 100;
-
-                red = (1 - cyan) * (1 - blackKey);
-                green = (1 - magenta) * (1 - blackKey);
-                blue = (1 - yellow) * (1 - blackKey);
-
-                RSlider.Value = (int)(red * 255);
-                GSlider.Value = (int)(green * 255);
-                BSlider.Value = (int)(blue * 255);
+                CalculateRGB();
 
                 FillRectangles();
             }
         }
-
+        #endregion
+        #region slider drag started/completed
         private void RSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
         {
             rgbDragged = true;
@@ -277,18 +248,163 @@ namespace Grafika
         {
             rgbDragged = false;
         }
+        #endregion
+        #region text box text changed
 
+        private void RTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(CheckIfSlidersLoaded() && !string.IsNullOrEmpty(RTextBox.Text) && rgbTyping)
+            {
+                red = Convert.ToDouble(RTextBox.Text) / 255;
 
-        //private void CalculateValues(int r, int g, int b, int c, int m, int y, int k)
-        //{
-        //    blackKey = new[] { 1 - red, 1 - green, 1 - blue }.Min();
-        //    cyan = (1 - red - blackKey) / (1 - blackKey);
-        //    magenta = (1 - green - blackKey) / (1 - blackKey);
-        //    yellow = (1 - blue - blackKey) / (1 - blackKey);
+                CalculateCMYK();
 
-        //    red = 1 - new[] { 1, cyan * (1 - blackKey) + blackKey }.Min();
-        //    green = 1 - new[] { 1, magenta * (1 - blackKey) + blackKey }.Min();
-        //    blue = 1 - new[] { 1, yellow * (1 - blackKey) + blackKey }.Min();
-        //}
+                FillRectangles();
+            }
+        }
+
+        private void GTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(GTextBox.Text) && rgbTyping)
+            {
+                green = Convert.ToDouble(GTextBox.Text) / 255;
+
+                CalculateCMYK();
+
+                FillRectangles();
+            }
+        }
+
+        private void BTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(BTextBox.Text) && rgbTyping)
+            {
+                blue = Convert.ToDouble(BTextBox.Text) / 255;
+
+                CalculateCMYK();
+
+                FillRectangles();
+            }
+        }
+
+        private void CTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(CTextBox.Text) && cmykTyping)
+            {
+                cyan = Convert.ToDouble(CTextBox.Text) / 255;
+
+                CalculateRGB();
+
+                FillRectangles();
+            }
+        }
+
+        private void MTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(MTextBox.Text) && cmykTyping)
+            {
+                magenta = Convert.ToDouble(MTextBox.Text) / 255;
+
+                CalculateRGB();
+
+                FillRectangles();
+            }
+        }
+
+        private void YTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(YTextBox.Text) && cmykTyping)
+            {
+                yellow = Convert.ToDouble(YTextBox.Text) / 255;
+
+                CalculateRGB();
+
+                FillRectangles();
+            }
+        }
+
+        private void KTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CheckIfSlidersLoaded() && !string.IsNullOrEmpty(KTextBox.Text) && cmykTyping)
+            {
+                blackKey = Convert.ToDouble(KTextBox.Text) / 255;
+
+                CalculateRGB();
+
+                FillRectangles();
+            }
+        }
+        #endregion
+        #region text box got/lost focus
+        private void RTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = true;
+        }
+
+        private void GTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = true;
+        }
+
+        private void BTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = true;
+        }
+
+        private void CTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = true;
+        }
+
+        private void MTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = true;
+        }
+
+        private void YTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = true;
+        }
+
+        private void KTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = true;
+        }
+
+        private void RTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = false;
+        }
+
+        private void GTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = false;
+        }
+
+        private void BTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            rgbTyping = false;
+        }
+
+        private void CTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = false;
+        }
+
+        private void MTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = false;
+        }
+
+        private void YTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = false;
+        }
+
+        private void KTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cmykTyping = false;
+        }
+        #endregion
     }
 }
